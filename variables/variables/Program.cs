44843703cadb98a822d8922c6c6a83d52dec8da2
AvanMyers <smail1341@gmail.com>
2022-08-05 16:39:38 +0300

    variables

---
+++ variables/variables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 28;
+            string name = "Vlad";
+            bool isOpen = true;
+            char letter = 'b';
+            float number = 97.3f;
+            short shortNumber = 947;
+            byte byteNumber = 7;
+            double doubleNumber = 36.7756;
+            long longNumber = 354612389;
+            uint price = 32454;
+        }
+    }
+}
